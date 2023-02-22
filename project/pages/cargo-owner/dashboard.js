7a16e851cde9a967a7c493a5d09d937d7ff214e6
Muhammad Haidar Izzuddin <muhammadhaidar@mail.ugm.ac.id>
2023-02-22 13:47:40 +0700

    fix: fixing dashboard page

--- project/pages/cargo-owner/dashboard.js
+++ project/pages/cargo-owner/dashboard.js
@@ -8,10 +8,10 @@ import Navbar from "@/components/Navbar";
 export default function dashboard() {
   return (
     <div><Navbar/>
-    <div className="w-screen h-screen bg-[#EBEFF2] p-36 pt-24">
+    <div className="w-screen bg-[#EBEFF2] p-36 pt-24">
       <p className="text-[52px] text-[#293165] font-bold mb-28">Cargo Owner Dashboard</p>
       <div className="flex justify-center space-x-24">
-        <div className="h-[550px] w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
+        <div className="w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
             <div className="h-[105px] items-center flex mb-12 py-10">
                 <Image className="py-10 scale-125" src={icon_create} alt="image"></Image>
             </div>
@@ -22,7 +22,7 @@ export default function dashboard() {
                 <p>3. Click send button to submit delivery order request</p>
             </div>
         </div>
-        <div className="h-[550px] w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
+        <div className="w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
             <div className="h-[105px] items-center flex justify-center mb-12 py-10">
                 <Image className="scale-125" src={icon_list} alt="image"></Image>
             </div>
@@ -32,7 +32,7 @@ export default function dashboard() {
                 <p>2. Select filter based on request that want to check or through searching</p>
             </div>
         </div>
-        <div className="h-[550px] w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
+        <div className="w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
             <div className="h-[105px] items-center flex mb-12 py-10">
                 <Image className="scale-125" src={icon_time} alt="image"></Image>
             </div>
--- project/pages/shipping-line/dashboard.js
+++ project/pages/shipping-line/dashboard.js
@@ -6,10 +6,10 @@ import icon_time from "../../assets/time.svg";
 
 export default function dashboard() {
   return (
-    <div className="w-screen h-screen bg-[#EBEFF2] p-36 pt-16">
+    <div className="w-screen bg-[#EBEFF2] p-36 pt-16">
       <p className="text-[52px] text-[#293165] font-bold mb-28">Shipping Line Dashboard</p>
       <div className="w-full flex items-center justify-center space-x-24 px-48">
-        <div className="h-[550px] w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
+        <div className="w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
             <div className="h-[105px] items-center flex justify-center mb-12 py-10">
                 <Image className="scale-125" src={icon_list} alt="image"></Image>
             </div>
@@ -19,7 +19,7 @@ export default function dashboard() {
                 <p>2. Select Search to view the applications to be analyzed and verified</p>
             </div>
         </div>
-        <div className="h-[550px] w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
+        <div className="w-full bg-white items-center flex flex-col p-16 rounded-xl border border-gray-400 transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-[1.005] hover:shadow-xl hover:shadow-slate-200 hover:bg-sky-50 duration-300 mb-5">
             <div className="h-[105px] items-center flex mb-12 py-10">
                 <Image className="scale-125" src={icon_time} alt="image"></Image>
             </div>
