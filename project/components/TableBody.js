4ba54519d34ce373b3737a9eaaf53f845f2a156d
Muhammad Iqbal Masykuri <muhammadiqbal2020@mail.ugm.ac.id>
2023-02-22 17:24:25 +0700

    feat(list-request): fix table body

--- project/components/TableBody.js
+++ project/components/TableBody.js
@@ -4,20 +4,32 @@ import { HiOutlineTrash } from "react-icons/hi2";
 
 export default function TableBody(props) {
   return (
-    <div className="grid grid-cols-6 w-full text-black">
-      <p className="px-10 py-4 bg-gray-100 inline-flex justify-center items-center border-r border-gray-300">
+    <div
+      className={`${
+        props.index % 2 === 0 ? "bg-gray-200" : "bg-gray-100"
+      } grid grid-cols-6 w-full text-black`}
+    >
+      <p className="px-10 py-4 inline-flex justify-center items-center border-r border-gray-300">
         {props.do_number}
       </p>
-      <p className="px-10 py-4 bg-gray-100 inline-flex justify-center items-center border-r border-gray-300">
+      <p className="px-10 py-4 inline-flex justify-center items-center border-r border-gray-300">
         {props.shipping_agency}
       </p>
-      <p className="px-10 py-4 bg-gray-100 inline-flex justify-center items-center border-r border-gray-300">
-        {props.status}
-      </p>
-      <p className="px-10 py-4 bg-gray-100 inline-flex justify-center items-center border-r border-gray-300">
+      <div className="px-10 py-4  inline-flex justify-center items-center border-r border-gray-300">
+        <p
+          className={`${
+            props.status === "On Progress" ? "bg-yellow-500" : ""
+          } ${
+            props.status === "Accepted" ? "bg-green-500" : "bg-red-500"
+          } px-4 py-1 rounded-full`}
+        >
+          {props.status}
+        </p>
+      </div>
+      <p className="px-10 py-4 inline-flex justify-center items-center border-r border-gray-300">
         {props.expired_date}
       </p>
-      <div className="col-span-2 bg-gray-100 justify-center flex px-5 py-3 space-x-20">
+      <div className="col-span-2 justify-center flex px-5 py-3 space-x-20">
         <button className="p-4 rounded-full text-indigo-800 bg-gray-300 hover:bg-indigo-800 hover:text-white">
           <IoDocumentTextOutline className="scale-150" />
         </button>
--- project/pages/cargo-owner/list.js
+++ project/pages/cargo-owner/list.js
@@ -6,7 +6,19 @@ const data = [
   {
     do_number: "1234567",
     shipping_agency: "blackphanter",
-    status: "pending",
+    status: "On Progress",
+    expired_date: "28-02-2023",
+  },
+  {
+    do_number: "9876543",
+    shipping_agency: "swansong",
+    status: "Rejected",
+    expired_date: "28-02-2023",
+  },
+  {
+    do_number: "0000123",
+    shipping_agency: "slytherin",
+    status: "Accepted",
     expired_date: "28-02-2023",
   },
 ];
@@ -15,51 +27,50 @@ export default function list() {
   return (
     <div className="flex flex-col bg-bgcolor w-screen h-screen">
       <Navbar />
-      <div className="p-32">
+      <div className="p-32 h-full flex flex-col">
         <div className="flex justify-between text-black">
           <p className="text-4xl font-bold">List Request Delivery Order</p>
           <input className="bg-white w-[24vw] border border-gray-300 drop-shadow-sm px-5 py-1"></input>
         </div>
-        <div className="mt-12">
-          <div className="w-max flex text-black bg-gray-400/80 rounded-t-xl font-semibold">
-            <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
-              Requested
-            </button>
-            <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
-              Done
-            </button>
-            <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
-              Expired
-            </button>
+        <div className="w-max flex text-black bg-gray-400/80 rounded-t-xl font-semibold mt-12 ">
+          <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
+            Requested
+          </button>
+          <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
+            Done
+          </button>
+          <button className="px-16 py-2 rounded-t-xl hover:bg-blue-300">
+            Expired
+          </button>
+        </div>
+        <div className="h-full bg-gray-400/80 flex flex-col w-full p-8">
+          <div className="font-semibold grid grid-cols-6">
+            <p className="px-10 py-2 bg-indigo-900 text-center items-center">
+              DO Number
+            </p>
+            <p className="px-10 py-2 bg-indigo-900 text-center items-center">
+              Shipping Agency
+            </p>
+            <p className="px-10 py-2 bg-indigo-900 text-center items-center">
+              Status
+            </p>
+            <p className="px-10 py-2 bg-indigo-900 text-center items-center">
+              Expired Date
+            </p>
+            <p className="px-10 py-2 bg-indigo-900 text-center col-span-2 items-center">
+              Action
+            </p>
           </div>
-          <div className="flex flex-col w-full px-10 py-5 bg-gray-400/80">
-            <div className="font-semibold grid grid-cols-6">
-              <p className="px-10 py-2 bg-indigo-900 text-center items-center">
-                DO Number
-              </p>
-              <p className="px-10 py-2 bg-indigo-900 text-center items-center">
-                Shipping Agency
-              </p>
-              <p className="px-10 py-2 bg-indigo-900 text-center items-center">
-                Status
-              </p>
-              <p className="px-10 py-2 bg-indigo-900 text-center items-center">
-                Expired Date
-              </p>
-              <p className="px-10 py-2 bg-indigo-900 text-center col-span-2 items-center">
-                Action
-              </p>
-            </div>
-            <div>
-              {data.map((item) => (
-                <TableBody
-                  do_number={item.do_number}
-                  shipping_agency={item.shipping_agency}
-                  status={item.status}
-                  expired_date={item.expired_date}
-                />
-              ))}
-            </div>
+          <div>
+            {data.map((item, index) => (
+              <TableBody
+                do_number={item.do_number}
+                shipping_agency={item.shipping_agency}
+                status={item.status}
+                expired_date={item.expired_date}
+                index={index + 1}
+              />
+            ))}
           </div>
         </div>
       </div>
